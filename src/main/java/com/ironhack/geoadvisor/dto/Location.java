c55fdf8d5a5533fd7b58e93a7785fe464aa640f0
rodriam <rodri.am90@gmail.com>
2022-12-12 23:17:29 +0100

    first commit

---
+++ src/main/java/com/ironhack/geoadvisor/dto/Location.java
@@ -0,0 +1,9 @@
+package com.ironhack.geoadvisor.dto;
+
+import java.math.BigDecimal;
+
+public class Location {
+    private String address;
+    private BigDecimal latitude ;
+    private BigDecimal longitude;
+}
---
+++ src/test/java/com/ironhack/geoadvisor/GeoadvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.geoadvisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoadvisorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
