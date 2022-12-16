cc672bf6bc180ae35d44ba5bc18b9b28bbbe36fb
AdrianTrigo <atrigo@outlook.com>
2022-12-16 20:32:20 +0100

    Distance

--- src/main/java/com/ironhack/geoadvisor/utils/Coordinates.java
+++ src/main/java/com/ironhack/geoadvisor/utils/Coordinates.java
@@ -10,7 +10,7 @@ import java.math.RoundingMode;
 
 public class Coordinates {
 
-    public BigDecimal coordinatesDistance(Location location1, Location location2){
+    /*public BigDecimal coordinatesDistance(Location location1, Location location2){
         // convert from degrees to radians
         var difLat = location1.getLatitude().subtract(location2.getLatitude());
         var difLon = location1.getLongitude().subtract(location2.getLongitude());
@@ -20,7 +20,10 @@ public class Coordinates {
         var result = powLat.add(powLon);
 
         return result.sqrt(new MathContext(10));
-    }
+    }*/
+
+
+
 
     public Location midPoint(Location... locations) {
         var midLatitude = new BigDecimal(0);
@@ -37,5 +40,27 @@ public class Coordinates {
         location.setLongitude(midLongitude);
         return location;
     }
+
+
+    public double coordinatesDistance(Location location1, Location location2){
+        var lat1 =location1.getLatitude().doubleValue();
+        var lng1 = location1.getLongitude().doubleValue();
+        var lat2 =location2.getLatitude().doubleValue();
+        var lng2 = location2.getLongitude().doubleValue();
+
+        double earthRadius = 6371.0;
+        double dLat = Math.toRadians(lat2-lat1);
+        double dLng = Math.toRadians(lng2-lng1);
+        double sindLat = Math.sin(dLat / 2);
+        double sindLng = Math.sin(dLng / 2);
+        double a = Math.pow(sindLat, 2) + Math.pow(sindLng, 2)* Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2));
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        double dist = earthRadius * c;
+
+        return dist;
+    }
+
+
+
 
 }
